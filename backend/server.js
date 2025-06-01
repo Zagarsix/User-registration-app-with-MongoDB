cf3b1d0b35bc88e7a25708ae535a390abd644ce0
Zagarsix <claudk1006@gmail.com>
2025-06-01 18:15:00 -0400

    initial configuration of base files

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
+
+//Conexión a MongoDB para que se cree automáticamente
+mongoose.connect('mongodb://localhost:27017/user-registration-db', {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Conexión exitosa a MongoDB'))
+.catch(err => console.error('No se ha podido establecer la conexión a MongoDB:', err));
