72318d4144b310489674893ab998c32301caf4e3
Zagarsix <claudk1006@gmail.com>
2026-04-21 21:33:44 -0400

    preparar app para deployment en Render

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
@@ -13,7 +15,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 //Conexión a MongoDB para que se cree automáticamente
-mongoose.connect('mongodb://localhost:27017/user-registration-db')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/user-registration-db')
 .then(() => console.log('Conexión exitosa a MongoDB'))
 .catch(err => console.error('No se ha podido establecer la conexión a MongoDB:', err));
 
